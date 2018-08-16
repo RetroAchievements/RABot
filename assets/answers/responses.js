b8b2e982982bd522de5baea6f900fc59e013ae48
meleu <meleu@users.noreply.github.com>
2018-08-16 15:44:01 +0000

    added gmorning messages

--- assets/answers/responses.js
+++ assets/answers/responses.js
@@ -1,13 +1,22 @@
 const hi = 'Hi there! 👋';
 const gnight = 'sleep well... 😴';
+const gmorning = 'good morning! 🌄';
 
 module.exports = {
     'hi': hi,
     'hi there': hi,
     'hello': hi,
+    'hello guys': hi,
+    'hi guys': hi,
 
     'good night here': gnight,
     'gnight': gnight,
     'g\'night here': gnight,
     'gnight here': gnight,
+    
+    'morning guys': gmorning,
+    'good morning': gmorning,
+    'good morning guys': gmorning,
+    'gmorning': gmorning,
+    'g\'morning': gmorning,
 }
