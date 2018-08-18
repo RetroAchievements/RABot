0e99c053abc9e41625d84ef738a79a87a08d5678
meleu <meleuzord@yahoo.com>
2018-08-18 20:46:03 -0300

    added pfp answer

--- assets/answers/responses.js
+++ assets/answers/responses.js
@@ -2,6 +2,8 @@ const hi = 'Hi there! 👋';
 const gnight = 'sleep well... 😴';
 const gmorning = 'good morning! 🌄';
 
+const pfp: "**So you've changed your profile picture on the site and it wasn't updated yet? You just need to clear the cache of your browser. Most of the cases hitting Ctrl+F5 is enough.**",
+
 module.exports = {
     'hi': hi,
     'hi there': hi,
@@ -19,4 +21,7 @@ module.exports = {
     'good morning guys': gmorning,
     'gmorning': gmorning,
     'g\'morning': gmorning,
+
+    '!pfp': pfp, // a workaround to pretend it's a command
+    'pfp': pfp,
 }
