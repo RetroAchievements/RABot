05ba5c19fdad562f41957f7dd1f301bc1938af73
meleu <meleuzord@yahoo.com>
2018-08-19 21:02:59 -0300

    added 'funfact' command

---
+++ commands/single/funfact.js
@@ -0,0 +1,18 @@
+const Command = require('../../structures/Command.js');
+const funfacts = require('../../assets/json/funfacts.json');
+
+module.exports = class FunFactCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'funfact',
+            aliases: ['ffact', 'funf'],
+            group: 'single',
+            memberName: 'funfact',
+            description: 'Responds with a retrogaming fun fact.'
+        });
+    }
+
+    run(msg) {
+        return msg.say('**' + funfacts[Math.floor(Math.random() * funfacts.length)] + '**');
+    }
+};
