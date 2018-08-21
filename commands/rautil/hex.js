46ccb0a26bec7fb00cf8b92d3addbd50f9ddd8e6
meleu <meleuzord@yahoo.com>
2018-08-21 00:00:12 -0300

    making 'hex' command a bit stronger

--- commands/rautil/hex.js
+++ commands/rautil/hex.js
@@ -6,7 +6,7 @@ module.exports = class HexCommand extends Command {
             name: 'hex',
             group: 'rautil',
             memberName: 'hex',
-            description: 'Converts an integer from decimal to hexadecimal (or vice-versa).',
+            description: 'Converts a non-negative integer from decimal to hexadecimal (or vice-versa).',
             examples: ['`hex 16`, `hex 0xaf 0x10`, `hex 4096 14 64`'],
             argsPromptLimit: 0,
             args: [
@@ -23,17 +23,27 @@ module.exports = class HexCommand extends Command {
     async run(msg, { numbers }) {
         let response = '```';
         let num;
+        let outOfRangeFlag = false;
 
         for(let i = 0; i < numbers.length; i++) {
             num = parseInt(numbers[i]);
 
             if(!isNaN(num))
-                response += `\n${num} = 0x${num.toString(16)}`;
+                if(num < 0) {
+                    response += `\n${num} = negative`;
+                    outOfRangeFlag = true;
+                } else if(num > 4294967295) { // 2^32
+                    response += `\n${numbers[i]} = TooBig!`;
+                    outOfRangeFlag = true;
+                } else { 
+                    response += `\n${num} = 0x${num.toString(16)}`;
+                }
             else
                 response += `\n${numbers[i]} = NaN`;
         }
 
         response += '```';
+        response += outOfRangeFlag ? '**Note**: use numbers between 0 and 2^32' : '';
         return msg.reply(response);
     }
 
