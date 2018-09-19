713a5ed7234dd275e5d4c2eacce4db34ae3e8f1b
meleu <meleu@users.noreply.github.com>
2018-09-19 11:09:19 -0300

    Create repo.js

---
+++ commands/helper/repo.js
@@ -0,0 +1,24 @@
+const HelperCommand = require('../../structures/HelperCommand.js');
+
+const { site, pages, answers } = require('../../assets/answers/faq.js');
+
+module.exports = class RepoCommand extends HelperCommand {
+    constructor(client) {
+        super(client, {
+            name: 'repo',
+            group: 'helper',
+            memberName: 'repo',
+            description: 'Provide links to RetroAchievements repositories.',
+            examples: ['`repo ralibretro`', '`repo raintegration`', '`repo wiki`'],
+            args: [
+                {   
+                    key: 'arg',
+                    prompt: '',
+                    type: 'string',
+                    default: 'default',
+                }
+            ]
+            }, site, pages, answers);
+    }
+
+};
