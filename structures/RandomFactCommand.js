4d39258434d37bc7de38c6d9129fda977f16ebbc
meleu <meleu@users.noreply.github.com>
2018-10-29 10:18:23 -0300

    show 'random' when providing an out-of-range value

--- structures/RandomFactCommand.js
+++ structures/RandomFactCommand.js
@@ -14,7 +14,7 @@ module.exports = class FunFactCommand extends Command {
         const length = this.facts.length;
 
         if( isNaN( num-- ) || num < 0 || num > length - 1 )
-            return msg.say('**' + this.facts[ Math.floor(Math.random() * length) ] + '**');
+            return msg.say( ( !number ? 'random: ' : '' ) '**' + this.facts[ Math.floor(Math.random() * length) ] + '**');
 
         return msg.say(`${num + 1}: **${this.facts[ num ]}**`);
     }
