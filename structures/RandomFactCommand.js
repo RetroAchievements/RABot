d12b68d7deef7b58166cbdc78f31fef19bc3fbf6
meleu <meleuzord@yahoo.com>
2018-10-29 20:55:19 -0300

    RandomFactCommand.js small improvements

--- structures/RandomFactCommand.js
+++ structures/RandomFactCommand.js
@@ -10,12 +10,20 @@ module.exports = class RandomFactCommand extends Command {
     }
 
     run(msg, { number }) {
-        let num = parseInt( number );
         const length = this.facts.length;
+        let num = parseInt( number );
+        let i;
+        let prefix = '';
 
-        if( isNaN( num-- ) || num < 0 || num > length - 1 )
-            return msg.say( ( !number ? 'random: ' : '' ) '**' + this.facts[ Math.floor(Math.random() * length) ] + '**');
+        if( isNaN( num-- ) || num < 0 || num > length - 1 ) {
+            i = Math.floor(Math.random() * length);
+            if( number != '~NOARGS~' ) prefix = 'random: ';
+            //return msg.say( ( number != ? 'random: ' : '' ) + '**' + this.facts[ Math.floor(Math.random() * length) ] + '**');
+        } else {
+            i = num;
+            prefix = `${num + 1}: `;
+        }
 
-        return msg.say(`${num + 1}: **${this.facts[ num ]}**`);
+        return msg.say(`${prefix}**${this.facts[ i ]}**`);
     }
 };
