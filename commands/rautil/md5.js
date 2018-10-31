60b279702525c1baef59fd8adcc9ba0c3e096df6
meleu <meleu@users.noreply.github.com>
2018-10-31 11:05:47 -0300

    md5 (#9)
    * Create md5.js
    
    * md5.js small fix

---
+++ commands/rautil/md5.js
@@ -0,0 +1,30 @@
+const Command = require('../../structures/Command.js');
+const crypto = require('crypto');
+
+module.exports = class Md5Command extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'md5',
+            group: 'rautil',
+            memberName: 'md5',
+            description: 'Generates an md5 hash for the strings given as arguments.',
+            examples: ['`md5 dino`', '`md5 mvsc`' ],
+            args: [
+                {
+                    key: 'terms',
+                    prompt: '',
+                    type: 'string',
+                    infinite: true,
+                    default: '1',
+                },
+            ]
+        });
+    }
+
+    run(msg, { terms } ) {
+        let reply = '';
+        terms.forEach(term => reply += `md5(\`${term}\`) = \`${crypto.createHash('md5').update(term).digest('hex')}\`\n`);
+
+        return msg.reply(reply);
+    }
+};
