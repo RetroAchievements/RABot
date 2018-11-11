fe576a103bd09e980db575e05d823665b107b210
meleu <meleu@users.noreply.github.com>
2018-11-11 21:32:25 -0200

    returning only data

--- util/Utils.js
+++ util/Utils.js
@@ -7,17 +7,16 @@ module.exports = class Utils {
         return /^[a-z0-9]{2,20}$/i.test(user);
     }
 
-    static async bestdays( user ) {
+    static async bestdays( user, days ) {
         if( !Utils.isValidUsername( user ) )
-            return `Invalid username: \`${user}\``;
+            return false;
 
-        const max = 3;
-        const url = 'https://retroachievements.org/history.php?c=' + max + '&u=' + user;
-        let response = `:trophy: __**${user}'s best days**__ :trophy:\n`;
-        response += `<${url}>`;
-        response += '```md\n';
+        const url = 'https://retroachievements.org/history.php?u=' + user;
+        let dates = [];
+        let cheevos = [];
+        let score = [];
 
-        const res = await fetch( url );
+        const res = await fetch( url + '&c=' + days)
         const $ = cheerio.load( await res.text() );
 
         const bestDays = $('table.smalltable').find('tr').map( (i, element) => ({
@@ -27,69 +26,16 @@ module.exports = class Utils {
         })).get();
 
         if(bestDays.length <= 2) {
-            response = `There's no info about \`${user}\``;
-            return;
+            return false;
         }
 
-        for(let i = 1; i <= max && i < bestDays.length; i++) {
-            response += `\n[${bestDays[i].date}]`;
-            response += `( ${bestDays[i].achievements} ) `;
-            response += `< ${bestDays[i].score} >`;
-        }
-        response += '\n```';
-
-        const bestScore = bestDays[1].score;
-        if(bestScore >= 3000) {
-            if(bestScore >= 10000)
-                response += "**Completely unreal score!**";
-            else if(bestScore >= 6000)
-                response += "**WOW!** This guy seems to play retrogames all day long!";
-            else if(bestScore >= 5000)
-                response += "That's a pretty dedicated retrogamer";
-            else
-                response += "That's good retrogamer!";
+        for(let i = 1; i <= days && i < bestDays.length; i++) {
+            date.push( bestDays[i].date );
+            cheevos.push( bestDays[i].achievements );
+            score.push( bestDays[i].score );
         }
 
-/*
-        fetch(url)
-            .then(res => res.text())
-            .then(body => {
-                const $ = cheerio.load(body);
-
-                const bestDays = $('table.smalltable').find('tr').map( (i, element) => ({
-                    date: $(element).find('td:nth-of-type(1)').text().trim(),
-                    achievements: $(element).find('td:nth-of-type(2)').text().trim(),
-                    score: $(element).find('td:nth-of-type(3)').text().trim(),
-                })).get();
-
-                if(bestDays.length <= 2) {
-                    response = `There's no info about \`${user}\``;
-                    return;
-                }
-
-                for(let i = 1; i <= max && i < bestDays.length; i++) {
-                    response += `\n[${bestDays[i].date}]`;
-                    response += `( ${bestDays[i].achievements} ) `;
-                    response += `< ${bestDays[i].score} >`;
-                }
-                response += '\n```';
-
-                const bestScore = bestDays[1].score;
-                if(bestScore >= 3000) {
-                    if(bestScore >= 10000)
-                        response += "**Completely unreal score!**";
-                    else if(bestScore >= 6000)
-                        response += "**WOW!** This guy seems to play retrogames all day long!";
-                    else if(bestScore >= 5000)
-                        response += "That's a pretty dedicated retrogamer";
-                    else
-                        response += "That's good retrogamer!";
-                }
-            });
-        // TODO: I think I'm supposed to .catch() something here, no?
-        */
-
-        return response;
+        return [ date, cheevos, score ];
     }
 
 };
