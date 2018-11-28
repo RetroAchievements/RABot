9a916ab27cfac32ac70a6b6416721d67b874eefd
meleu <meleu@users.noreply.github.com>
2018-11-28 12:53:15 -0200

    Create developer-news.js

---
+++ commands/mod/developer-news.js
@@ -0,0 +1,26 @@
+const SelfRoleCommand = require('../../structures/SelfRoleCommand.js');
+
+require('dotenv').config({path: __dirname + '/.env'});
+const { DEVELOPER_NEWS } = process.env;
+
+
+module.exports = class DeveloperNewsCommand extends SelfRoleCommand {
+    constructor(client) {
+        super(client, {
+            name: 'developer-news',
+            group: 'mod',
+            aliases: ['developernews', 'dev-news', 'devnews'],
+            memberName: 'developer-news',
+            description: 'Add/Remove the `@dev-news` role to/from yourself',
+            argsPromptLimit: 0,
+            args: [
+                {
+                    key: 'action',
+                    type: 'string',
+                    prompt: '',
+                    default: 'show',
+                },
+            ],
+        }, DEVELOPER_NEWS);
+    }
+};
