6bec32fe9901cbcdc570a9b07f5dcbf605c4b3a7
meleu <meleuzord@yahoo.com>
2018-12-19 16:43:18 -0200

    added 'define' command

---
+++ commands/search/define.js
@@ -0,0 +1,48 @@
+/*
+ * This code is originally from https://github.com/dragonfire535/xiao/
+ */
+const Command = require('../../structures/Command');
+const request = require('node-superfetch');
+const { stripIndents } = require('common-tags');
+const { WORDNIK_KEY } = process.env;
+
+module.exports = class DefineCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'define',
+            aliases: ['dictionary', 'dict'],
+            group: 'search',
+            memberName: 'define',
+            description: 'Defines a word.',
+            args: [
+                {
+                    key: 'word',
+                    prompt: 'What word would you like to look up?',
+                    type: 'string',
+                    parse: word => encodeURIComponent(word)
+                }
+            ]
+        });
+    }
+
+    async run(msg, { word }) {
+        try {
+            const { body } = await request
+                .get(`http://api.wordnik.com/v4/word.json/${word}/definitions`)
+                .query({
+                    limit: 1,
+                    includeRelated: false,
+                    useCanonical: true,
+                    api_key: WORDNIK_KEY
+                });
+            if (!body.length) return msg.say('Could not find any results.');
+            const data = body[0];
+            return msg.say(stripIndents`
+                **${data.word}**
+                (${data.partOfSpeech || 'unknown'}) ${data.text}
+            `);
+        } catch (err) {
+            return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+        }
+    }
+};
