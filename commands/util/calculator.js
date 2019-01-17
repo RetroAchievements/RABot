f1ff4c4f8450540665ca903ebf46f4f9edd8774b
meleu <meleu@users.noreply.github.com>
2019-01-17 08:37:55 -0200

    added 'calculator' command - closes #22 (#23)
    * added 'calculator' command - closes #22
    
    needs more tests.
    
    * added "mathjs" package as dependency

---
+++ commands/util/calculator.js
@@ -0,0 +1,30 @@
+const Command = require('../../structures/Command');
+const mathjs = require('mathjs');
+
+module.exports = class CalculatorCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'calculator',
+            aliases: ['calc'],
+            group: 'util',
+            memberName: 'calculator',
+            description: 'Evaluates the given math expression and responds with the result.',
+            examples: ['`calculator 2 + 5`', '`calc 2.3 * 4.5`', '`calc sqrt(3^2 + 4^2)`', '`calc cos(45 deg)`' ],
+            args: [
+                {
+                    key: 'expression',
+                    prompt: 'What expression do you want to get the result?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    run(msg, { expression }) {
+        try {
+            return msg.reply('`' + mathjs.eval( expression ) + '`');
+        } catch (err) {
+            return msg.reply(`Oh no, an error occurred: \`${err.message}\`! :frowning:`);
+        }
+    }
+};
