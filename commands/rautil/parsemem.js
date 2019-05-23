3ec307bfba16543844b350d04ec917a9b41092c7
meleu <meleu@users.noreply.github.com>
2019-05-23 13:43:21 -0300

    support for AndNext and Prior in !parsemem

--- commands/rautil/parsemem.js
+++ commands/rautil/parsemem.js
@@ -18,6 +18,7 @@ const specialFlags = {
     'a': 'AddSource',
     'b': 'SubSource',
     'c': 'AddHits',
+    'n': 'AddNext',
     '' : ''
 };
 
@@ -42,6 +43,7 @@ const memSize = {
 
 const memTypes = {
     'd': 'Delta',
+    'p': 'Prior',
     'm': 'Mem',
     'v': 'Value',
     '' : ''
