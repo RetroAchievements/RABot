f13718bc694948a7e5d15df295a03b249a725d94
meleu <meleuzord@yahoo.com>
2019-05-25 19:57:00 -0300

    update !rarch badges

--- assets/answers/rarch.js
+++ assets/answers/rarch.js
@@ -10,7 +10,7 @@ const nightly = '**Download the most up to date version of RetroArch here:\n<htt
 
 const specific = '**We have a #retroarch channel, but for questions completely unrelated to RetroAchievements, maybe you can get more help at libretro discord server:\n<https://discordapp.com/invite/chjzzQx>**';
 
-const badges = '**When playing on RetroArch you do NOT have the feature that shows the cheevo\'s badge when you complete it.**\nMore info here: <http://docs.retroachievements.org/FAQ/#how-can-i-see-the-badge-when-i-earn-a-cheevo-on-retroarch>';
+const badges = "This feature was implemented in RetroArch 1.7.7 (May/2019), however there are some platforms where it still doesn't work (such as PS Vita). You just need to assure the Menu Widget option enabled (and it's usually enabled by default)."
 
 const def = `**With RetroArch you can earn RetroAchievements on a wide range of computers and consoles.**\nMore info here: <${site}>`;
 
