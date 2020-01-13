4719f885d92ed34948750759ff1e0a5079347507
meleu <meleuzord@yahoo.com>
2020-01-13 13:54:42 -0300

    add !frames command, closes #40

---
+++ commands/rautil/frames.js
@@ -0,0 +1,84 @@
+const Command = require('../../structures/Command.js');
+
+module.exports = class FramesCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'frames',
+            group: 'rautil',
+            memberName: 'frames',
+            description: 'Says how many frames corresponds to a given amount of time or the vice-versa (see Examples below).',
+            examples: [
+                '`frames 1h 5min 15s` - info for 1 hour, 5 minutes and 15 seconds considering 30 frames/second (default)',
+                '`frames 500ms 60fps` - info for 500 milliseconds, considering 60 frames/second',
+                '`frames 123.321min 25fps` - info for 123.321 minutes, considering 25 frames/second',
+                '`frames 40` - info for 40 frames, considering 30 frames/second (default)',
+                '`frames 123 60fps` - info for 123 frames, considering 60 frames/second',
+            ],
+            args: [
+                {
+                    key: 'input',
+                    prompt: '',
+                    type: 'string',
+                    infinite: true,
+                },
+            ]
+        });
+    }
+
+    run( msg, { input }) {
+        // group2: hours
+        // group4: minutes
+        // group6: seconds
+        // group8: milliseconds
+        const regexTime = /(([0-9.]+) *h)? *(([0-9.]+) *min)? *(([0-9.]+) *s)? *(([0-9]+) *ms)?/i;
+
+        // group1: frames per second
+        const regexFps = /([0-9]+) *fps/i;
+
+        // group2: frames amount
+        const regexFramesAmount = /^ *([0-9]+ *fps )? *([0-9]+) *( [0-9]+ *fps)? *$/
+
+        const inputString = input.join(' ');
+
+        const parsedTime = inputString.match(regexTime);
+        const parsedFps  = inputString.match(regexFps);
+
+        let totalSeconds = 0;
+        let fps, frames;
+        let hours, minutes, seconds, milliseconds;
+
+        fps = parsedFps ? parseFloat(parsedFps[1]) : 30;
+        hours        = parseFloat(parsedTime[2] || 0);
+        minutes      = parseFloat(parsedTime[4] || 0);
+        seconds      = parseFloat(parsedTime[6] || 0);
+        milliseconds = parseFloat(parsedTime[8] || 0);
+        totalSeconds = seconds + milliseconds / 1000 + minutes * 60 + hours * 60 * 60;
+
+        if (totalSeconds > 0 && fps > 0) {
+            frames = Math.round(fps * totalSeconds);
+        } else {
+            const parsedFramesAmount = inputString.match(regexFramesAmount);
+
+            frames = parsedFramesAmount ? parseInt(parsedFramesAmount[2]) : 0;
+
+            if (frames <= 0 || fps <= 0) {
+                msg.reply('invalid time format: `' + inputString + '`\nUse `!help frames` to se some useful examples');
+                return;
+            }
+
+            totalSeconds = parseFloat(frames / fps);
+        }
+
+        // deal with the milliseconds first and then remove it from totalSeconds
+        milliseconds = Math.round(1000 * (totalSeconds - Math.floor(totalSeconds)));
+        totalSeconds = Math.floor(totalSeconds);
+
+        hours   = Math.floor(totalSeconds / 3600);
+        minutes = Math.floor((totalSeconds - hours * 3600) / 60);
+        seconds = totalSeconds - hours * 3600 - minutes * 60;
+
+        msg.say(`**Time:** \`${hours}h ${minutes}min ${seconds}s ${milliseconds}ms\`` +
+            `\n**FPS:** \`${fps}\`` +
+            `\n**Frames:** \`${frames} (0x${frames.toString(16)})\``);
+    }
+}
