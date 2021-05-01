392e9bbb268eabeb530f7ae51995c0aac1a545a4
Augusto Lopes <augusto.lopes@sysmap.com.br>
2021-05-01 17:07:32 -0300

    updated 'simple rules'

--- assets/answers/rules.js
+++ assets/answers/rules.js
@@ -1,7 +1,9 @@
 const one = "**1.** Don't be a jerk or generally obnoxious - nobody likes trolls.";
-const two = '**2.** Do NOT use this server to share copyrighted material or info about where they can be downloaded.';
-const three = '**3.** Keep the channels on-topic.';
-const four = '**4.** When in doubt, ask a @mod.';
+const two = "**2.** Don't use our website or Discord server to share copyrighted material or information about where they can be downloaded.";
+const three = "**3.** Keep the Discord channels and forum threads on-topic (we do have a section for off-topic chatting, though).";
+const four = "**4.** When a moderator/admin asks you to stop, you should stop.";
+const five = "**5.** When in doubt, ask a @mod";
+
 
 const coc = '**Complete Version**: <http://docs.retroachievements.org/Users-Code-of-Conduct/>';
 
@@ -10,7 +12,8 @@ module.exports = {
   2: two,
   3: three,
   4: four,
+  5: five,
   coc,
 
-  all: `__**RULES**__\n**Simple Version**:\n${one}\n${two}\n${three}\n${four}\n\n${coc}`,
+  all: `__**RULES**__\n**Simple Version**:\n${one}\n${two}\n${three}\n${four}\n${five}\n\n${coc}`,
 };
