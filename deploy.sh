371448ebd49a9753691656dfe6a7b825ce641541
luchaos <luchaos@gmail.com>
2023-11-10 01:48:22 +0100

    chore: remove deploy script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-ssh -T deploy-discord-bot@retroachievements.org << EOF
-    cd /srv/discord-bot
-    git pull -v
-    npm i --production
-    sudo supervisorctl restart discord-bot:*
-EOF
